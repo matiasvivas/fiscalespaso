f5c6b197e70d28b099a97fd4bb1ffc57172fb565
matias.vivas <matiasvivas@hotmail.com>
2023-08-13 13:02:06 -0300

    Bug 7 role

--- src/main/java/pl/codeleak/demos/sbt/service/UserService.java
+++ src/main/java/pl/codeleak/demos/sbt/service/UserService.java
@@ -36,7 +36,7 @@ public class UserService {
 
     public User saveUser(User user) {
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-        Role userRole = roleRepository.findByRole("");
+        Role userRole = roleRepository.findByRole("FISCAL");
         user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
         return userRepository.save(user);
     }
